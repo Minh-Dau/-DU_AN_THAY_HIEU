dd66830cd38e16dd0dc402f6bbebaedc9d10808d
Minh-Dau <tranminhdau85@gmail.com>
2024-10-31 20:21:11 +0700

    hihi

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,17 @@ class HomeController < ApplicationController
   def register
     # Renders the registration form
   end
-
+  def trangchu
+    firebase = FirebaseService.new
+    @user = session[:user]
+    # Lấy thông tin người dùng từ session
+    user_id = session[:user_id]
+    @user = firebase.find_user_by_id(user_id)
+  
+    unless @user
+      redirect_to login_path, alert: "Bạn cần đăng nhập để truy cập trang này."
+    end
+  end
   def create
     firebase_service = FirebaseService.new
     email = params[:email]
